2ae0e83a2d296ff9c78b6be50976bfb180596c95
Agustin <comteagustinignacio@gmail.com>
2021-07-01 21:17:02 -0300

    Datatypes

---
+++ basic-functions.py
@@ -0,0 +1,6 @@
+# Show the parameters into the command line
+print('example') # > "example"
+print(2) # > 2
+
+# Return the datatype of the parameters
+type('hello world') # > class 'str'
---
+++ datatype.py
@@ -0,0 +1,57 @@
+# Strings
+"Hello World"
+'hello World'
+"""Hello World"""
+'''Hello World'''
+
+'good' + 'morning' # > "good morning"
+'hello' + 'world' # > "hello world"
+
+# Integers
+2
+30
+100
+2000
+
+# floats
+3.2
+5.1
+9.8
+100.2
+90.888
+
+# Boolean (True and False, must be start with capital letter)
+True
+False
+
+# Lists (can be modify)
+[10, 20, 43, 50, 23]
+['hello', 'bye', 'hi']
+[10, 'hello', True, 1.2]
+
+# Tuples (can't be modify)
+(10, 20, 43, 50, 23)
+('hello', 'bye', 'hi')
+(10, 'hello', True, 1.2)
+
+# Dictionaries (its too similar to json)
+
+# {
+#    key: value
+# }
+
+{
+    'name': 'Agustín',
+    'lastname': 'Comte',
+    'nickname': 'Cholo'
+}
+{
+    'lat': 121212314213,
+    'long': 12323123
+}
+{
+    'status': True
+}
+
+# None (Means that the element it's not defined)
+None
