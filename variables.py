643be5f8906a8df2d3f8292bc63b3e4e3352aa71
Agustin <comteagustinignacio@gmail.com>
2021-07-02 17:09:26 -0300

    Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Variables
+name = 'agustin'
+print(name) # > "agustin"
+
+name = 2
+print(name) # > 2
+
+# Case sensitive
+name = 'carlos'
+Name = 'juan'
+
+# Conventions
+# Variables must be start with _ or letter
+# The word in the variables can be separate with camelCase and _
+# variables that be written in uppercase cant be modify
