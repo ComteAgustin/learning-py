1812f7a57a07be58a0fa0a48b23421967da98d78
Agustin <comteagustinignacio@gmail.com>
2021-07-03 02:54:54 -0300

    loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+# Loops
+foods = ['apples', 'bread', 'milk']
+
+# for, repeat as many times as elements in the list
+for food in foods:
+    print(food) # > "apples", "bread", "milk"
+
+# While, be repeating the action while the condition is true
+count = 4
+
+while count <= 10:
+    print(count)
+    count = count + 1 # have to has a code where the counter have to increment
